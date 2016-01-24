6a315c5480798d168341665e4fff1f6613419c7f
reabelyl <reabelyl@live.com>
2016-01-24 15:36:49 -0500

    Version 2
    Edited

--- main.cpp
+++ main.cpp
@@ -9,38 +9,54 @@
 //
 //
 
+#include <stdio.h>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 // Main Script
-int main
+int main()
 {
-    // Input number 1-100
-    cout << "Enter Number from 1 to 100: "
     int a;
-    cin >> a;
     
-    // Enter code check
-    if (a>100)
+    do
     {
-        Break
+        cout << "Enter Number from 1 to 100: ";
+        int a;
+        cin >> a;
     }
-    
-    // Script to sort number to word
-    
-    //
-    if (a%4==0)
-    // {
-    // 4 & 7 "Spaniel"
-    // }
-    // else
-    // {
-        //4 “Cavalier”
-    
-        //7 “King Charles”
-    // }
+        while (a > 1 && a < 100);
+        {
+            // Script to sort number to word
+            {
+                if (a%4 && a%7)
+                {
+                    cout << "\n\n Cavalier \n\n";
+                    cout << "\n\n King Charles \n\n";
+                }
+            }
+        }
+        if (a>100)
+        {
+            // Check for Valid Number
+            if (a>100)
+            {
+                // Error Message
+                cout << "\n\n\nError: Please Enter a number between 1 and 100.\n\n\n";
+            }
+        }
+        // end of do loop
+        
+        return 0;
+    }
 
 
-    return 0;
-};
+//HOW DO I MAKE A MULTIPLE CHECK IF STATEMENT?
+//if ()
+// {
+// 4 & 7 "Spaniel"
+// }
+// else
+// {
+//4 “Cavalier”
+//7 “King Charles”

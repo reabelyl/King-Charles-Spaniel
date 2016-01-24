d27c02b557135f428b80b97ada03ad9bbb15e9a5
reabelyl <reabelyl@live.com>
2016-01-24 13:58:57 -0500

    Ex2 Draft3
    Exercise 2 Draft 3

---
+++ main.cpp
@@ -0,0 +1,46 @@
+//
+//  main.cpp
+//  King Charles Spaniel Script
+//
+//  Created by Lyle Baer on 1/24/16.
+//  Copyright © 2016 Lyle Baer. All rights reserved.
+//  Input:
+//  Output:
+//
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Main Script
+int main
+{
+    // Input number 1-100
+    cout << "Enter Number from 1 to 100: "
+    int a;
+    cin >> a;
+    
+    // Enter code check
+    if (a>100)
+    {
+        Break
+    }
+    
+    // Script to sort number to word
+    
+    //
+    if (a%4==0)
+    // {
+    // 4 & 7 "Spaniel"
+    // }
+    // else
+    // {
+        //4 “Cavalier”
+    
+        //7 “King Charles”
+    // }
+
+
+    return 0;
+};
